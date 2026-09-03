cb724ce2f35d52933f50f53ed6e6bec98752091b
ArseniyO <aohorilko22@gmail.com>
2026-09-03 12:06:29 -0400

    Making a .gitignore file to practice for COMP3090. got no fucking clue what im doing and hope this works

--- starter.ml
+++
@@ -1,5 +0,0 @@
-(* This is an OCaml editor.
-   Enter your program here and send it to the toplevel using the "Eval code"
-   button or [Ctrl-e]. *)
-
-(if true then 100 else 0) + (if false then 0 else 23);;
